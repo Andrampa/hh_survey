899e8da20ee28f383d5a83a8c45f89a1ead2cb2b
Andrampa <andrea.amparore@gmail.com>
2021-05-06 11:10:23 +0200

    generalized script

---
+++ scripts/compare_two_table_structures.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from xlsxwriter import Workbook
+from two_lists_similarity import Calculate_Similarity as cs
+import os
+
+
+geopoll_or_kobo_template = r'C:\temp\kobo_template_table.xlsx' #include the word 'kobo' or 'geopoll' inside the filename
+master_table_template = r'C:\temp\master_table_template.xlsx'
+output_folder = r'C:/temp'
+
+
+country_df = pd.read_excel(open(geopoll_or_kobo_template, 'rb'))
+if "kobo" in geopoll_or_kobo_template.lower():
+    country_df.columns = country_df.columns.str.replace("[/]", "")
+    print("Opening kobo questionnaire")
+elif "geopoll" in geopoll_or_kobo_template.lower():
+    print("Opening geopoll questionnaire")
+else:
+    print("Please include Geopoll or Kobo inside the filename: %s " % geopoll_or_kobo_template)
+    exit()
+
+print("Opening master table")
+global_df = pd.read_excel(open(master_table_template, 'rb'))
+
+country_cols = country_df.columns
+global_cols = global_df.columns
+
+
+common_cols = list(country_cols.intersection(global_cols))
+country_not_global = list(country_cols.difference(global_cols))
+global_not_country = list(global_cols.difference(country_cols))
+
+
+Column1 = country_not_global
+Column2 = global_not_country
+
+from xlsxwriter import Workbook
+output_file = os.path.join(output_folder,'table_structure_comparison.xlsx')
+workbook = Workbook(output_file)
+
+stats_Sheet=workbook.add_worksheet()
+
+stats_Sheet.write(0, 0, 'Columns matching: %s' % len(common_cols))
+stats_Sheet.write(1, 0, 'columns in geopoll/kobo template and not in master: %s' % len(country_not_global))
+stats_Sheet.write(2, 0, 'columns in in master and not in geopoll/kobo template: %s' % len(global_not_country))
+
+Report_Sheet=workbook.add_worksheet()
+
+Report_Sheet.write(0, 0, 'common_cols')
+Report_Sheet.write(0, 1, 'columns in geopoll/kobo template and not in master')
+Report_Sheet.write(0, 2, 'columns in in master and not in geopoll/kobo template')
+
+
+Report_Sheet.write_column(1, 0, common_cols)
+Report_Sheet.write_column(1, 1, country_not_global)
+Report_Sheet.write_column(1, 2, global_not_country)
+workbook.close()
+print("Check output table with structure comparison: ", output_file)
+
+
